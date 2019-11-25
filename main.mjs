905cde9a556a11b6c0ce9a4cdd973d934d89f558
Ben Potter <benjamin.potter@live.com>
2019-11-25 16:48:48 -0700

    Modifiers and sub scripts working

--- main.mjs
+++ main.mjs
@@ -2,6 +2,14 @@ import Tesseract from "tesseract.js";
 import Jimp from "jimp";
 import fs from "fs"
 
+import translations from "./translations";
+
+
+let sourceKey = translations['D'];
+
+
+const file = 't1.jpg';
+
 const worker = Tesseract.createWorker({
     logger: m => console.log(m)
 });
@@ -11,31 +19,86 @@ const worker = Tesseract.createWorker({
     await worker.loadLanguage('eng');
     await worker.initialize('eng');
     await worker.setParameters({
-        "preserve_interword_spaces": "0",
+        "preserve_interword_spaces": "1",
         "tessjs_create_hocr": "1",
         "tessjs_create_tsv": "0",
         "tessjs_create_unlv": "0"
     });
 
-    let file = await worker.recognize('t1.jpg');
+    let ocr = await worker.recognize(file);
 
-    Jimp.read('t1.jpg')
+    Jimp.read(file)
         .then(image => {
 
             let result = image.clone();
-            file.data.lines.forEach(line => {
+
+            result.write('out.png');
+
+            ocr.data.lines.forEach(line => {
                 line.words.forEach(word => {
-                    if (word.text === "D") {
-                        new Jimp((word.bbox.x1 - word.bbox.x0) + 10, (word.bbox.y1 - word.bbox.y0) + 10, Jimp.cssColorToHex("#0000ff"), (err, value) => {
-                            result.composite(value, word.bbox.x0 - 5, word.bbox.y0 - 5);
-                            result.write('out.jpg');
+                    let box = word;
+
+                    let text = box.text.replace(/\[/g, "").trim();
+
+                    let found = null;
+                    Object.keys(sourceKey).forEach(key => {
+                        if (key === text)
+                            found = sourceKey[key];
+                        else
+                            translations.modifiers.forEach(modifier => {
+                                if ((key + modifier) === text) {
+                                    let sourceKeyElement = sourceKey[key];
+                                    found = {n: sourceKeyElement.n, modifier: modifier};
+                                }
+                            })
+                    });
+
+                    if (!found) {
+                        Object.keys(sourceKey).forEach(key1 => {
+                            Object.keys(sourceKey).forEach(key2 => {
+                                if ((key1 + key2) === text) {
+                                    found = {n: sourceKey[key1].n + "/" + sourceKey[key2].n};
+                                }
+                            });
                         });
+                    }
+
+                    if (found) {
+                        new Jimp(
+                            (box.bbox.x1 - box.bbox.x0) + 30,
+                            (box.bbox.y1 - box.bbox.y0) + 10,
+                            Jimp.cssColorToHex("#ffffff"),
+                            (err, value) => {
+                                result.composite(value, box.bbox.x0 - 5, box.bbox.y0 - 5);
+
+                                Jimp.loadFont(Jimp.FONT_SANS_32_BLACK)
+                                    .then(font => {
+                                        result.print(
+                                            font,
+                                            box.bbox.x0,
+                                            box.bbox.y0,
+                                            found.n,
+                                            50,
+                                            (err, image, {x, y}) => {
+                                                if (found.modifier) {
+                                                    image.print(
+                                                        font,
+                                                        x,
+                                                        y - Jimp.measureTextHeight(font, found.n, 100) - 10,
+                                                        found.modifier
+                                                    );
+                                                }
+                                            }
+                                        );
+                                        result.write('out.png');
+                                    });
+                            });
                     }
                 })
             });
         });
 
-    fs.writeFileSync("hocr.html", file.data.hocr + "<script src=\"https://unpkg.com/hocrjs\"></script>");
+    fs.writeFileSync("hocr.html", ocr.data.hocr + "<script src=\"https://unpkg.com/hocrjs\"></script>");
 
     await worker.terminate();
 })();
